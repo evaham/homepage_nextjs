18c8a983df28d884188ac66fc25f37554ac6e23e
JungWoo Ham <evaham@naver.com>
2024-04-18 16:41:32 +0900

    240418

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,54 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const resp = await fetch('http://localhost:9999/list_group1/', { cache: 'no-store'});
+  const topics = await resp.json();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="">
+      <div className="">
+        <div className="max-w-7xl mx-auto py-14 grid grid-cols-4 grid-flow-row gap-10">
+          <div className="relative flex flex-col items-center justify-center p-3 col-start-1 col-end-3 row-start-1 row-end-3">
+            <p className="text-4xl dark:sm:text-slate-400">매장운영의 기본 중 기본!</p>
+            <p className="mt-1 text-6xl font-semibold dark:sm:text-white">투게더 포스 솔루션</p>
+            <p className="mt-1 text-sm leading-6 col-start-1 sm:col-span-2 xl:mt-6 xl:row-start-4 xl:col-span-1 dark:text-slate-400">
+              일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십
+            </p>
+          </div>
+          {topics.map((list_group1)=>{
+            return <div className=" h-60 p-6 rounded-2xl bg-slate-200 dark:bg-slate-800 cursor-pointer transition ease-in-out delay-30 hover:-translate-y-1 hover:scale-110" key={list_group1.id}><a href={`/read/${list_group1.id}`} className="">{list_group1.title}</a></div>
+          })}
+        </div>
+      </div>
+      <div className="bg-slate-100">
+        <div className="max-w-7xl mx-auto py-14 grid grid-cols-4 grid-flow-row gap-10">
+          <div className="relative flex flex-col items-center justify-center p-3 col-start-3 col-end-5 row-start-1 row-end-3">
+            <p className="text-4xl dark:sm:text-slate-400">매장운영의 기본 중 기본!</p>
+            <p className="mt-1 text-6xl font-semibold dark:sm:text-white">투게더 포스 솔루션</p>
+            <p className="mt-1 text-sm leading-6 col-start-1 sm:col-span-2 xl:mt-6 xl:row-start-4 xl:col-span-1 dark:text-slate-400">
+              일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십
+            </p>
+          </div>
+          {topics.map((list_group1)=>{
+            return <div className="h-60 p-6 rounded-2xl bg-slate-300" key={list_group1.id}><a href={`/read/${list_group1.id}`}>{list_group1.title}</a></div>
+          })}
+        </div>
+      </div>
+      <div className="">
+        <div className="max-w-7xl mx-auto py-14 grid grid-cols-4 grid-flow-row gap-10">
+          <div className="relative flex flex-col items-center justify-center p-3 col-start-2 col-end-4 row-start-1 row-end-3">
+            <p className="text-4xl dark:sm:text-slate-400">매장운영의 기본 중 기본!</p>
+            <p className="mt-1 text-6xl font-semibold dark:sm:text-white">투게더 포스 솔루션</p>
+            <p className="mt-1 text-sm leading-6 col-start-1 sm:col-span-2 xl:mt-6 xl:row-start-4 xl:col-span-1 dark:text-slate-400">
+              일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십
+            </p>
+          </div>
+          {topics.map((list_group1)=>{
+            return <div className="h-60 p-6 border rounded-2xl bg-slate-200 dark:bg-slate-800" key={list_group1.id}><a href={`/read/${list_group1.id}`}>{list_group1.title}</a></div>
+          })}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
