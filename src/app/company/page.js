c7a45ea9415dd827f709a701de84d2e06b86c80f
JungWoo Ham <evaham@naver.com>
2024-04-29 16:57:14 +0900

    20240429

--- src/app/company/page.js
+++ src/app/company/page.js
@@ -1,18 +1,95 @@
-import Image from "next/image";
+'use client'
 
-export default async function Company () {
-  const resp = await fetch('http://localhost:9999/list_group1/', { cache: 'no-store'});
-  const topics = await resp.json();
+import { useState, useEffect } from "react";
+
+export default function Company() {
+  // API에서 받아온 데이터를 저장할 상태
+  const [data, setData] = useState([]);
+  // 필터링된 데이터를 저장할 상태
+  const [filteredData, setFilteredData] = useState([]);
+  // 검색어 상태
+  const [searchQuery, setSearchQuery] = useState('');
+
+  // 컴포넌트가 마운트될 때 한번만 실행
+  useEffect(() => {
+    // API로부터 데이터를 가져오는 비동기 함수
+    const fetchData = async () => {
+      try {
+        // API 호출하여 데이터 가져오기
+        const response = await fetch('http://localhost:9999/list_group1/');
+
+        // JSON 형식으로 파싱하여 데이터 저장
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    // fetchData 함수 실행
+    fetchData();
+  }, []);
+
+
+  // 검색어 또는 데이터가 변경될 때마다 실행
+  useEffect(() => {
+    if (!searchQuery) {
+      // 검색어가 없으면 모든 데이터를 보여줌
+      setFilteredData(data);
+      return;
+    }
+    // 검색어가 변경될 때마다 필터링된 데이터를 업데이트
+    const filtered = data.filter(item =>
+      // 검색어가 포함된 데이터만 필터링 하여 반환
+      item.cate.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    
+    setFilteredData(filtered);
+  }, [searchQuery, data]);
+
+  // 검색어 입력값이 변경될 때 실행되는 함수
+  const handleSearchInputChange = event => {
+    setSearchQuery(event.target.value);
+  };
+
+  const tab = [
+    {id: 1, cate: "", search : "", name : "전체"},
+    {id: 2, cate: "포스솔루션", search : "포스솔루션", name : "포스솔루션"},
+    {id: 3, cate: "모바일솔루션", search : "모바일솔루션", name : "모바일솔루션"}
+  ];
+
+  const [selectedTab, setSelectedTab] = useState("");
+
+  const handleTabClick = (e) => {
+    setSelectedTab (e)
+    setSearchQuery (e);
+  };
+
 
   return (
-    <main >
-      <div className="bg-slate-100">
-        <div className="max-w-7xl mx-auto py-14 grid grid-cols-4 grid-flow-row gap-10">
-          {topics.map((list_group1)=>{
-            return <div className="h-60 p-6 rounded-2xl bg-slate-300 cursor-pointer transition ease-in-out delay-30 hover:-translate-y-1 hover:scale-110" key={list_group1.id}><a href={`/read/${list_group1.id}`}>{list_group1.title}</a></div>
-          })}
-        </div>
+    <div className="flex max-w-7xl mx-auto py-14">
+      <ul className="flex flex-col items-start w-2/5 mr-8" >
+        {tab.map((tab)=>{
+          return (
+            <li key={tab.id} onClick={() => handleTabClick(tab.cate)} className={`border rounded-full mt-2 px-6 py-2 text-sm cursor-pointer ${selectedTab === tab.cate ? 'bg-slate-600' : ''}`}>{selectedTab}/{tab.cate}{tab.name}</li>
+          )
+        })}
+        {/* <input className="" type="text" placeholder="Search..." value={searchQuery} onChange={handleSearchInputChange} /> */}
+      </ul>
+      <div className="grid grid-cols-3 grid-flow-row gap-10">
+        {filteredData.map(item => (
+          <a key={item.id} className="overflow-hidden flex flex-col h-80 rounded-2xl border border-slate-100 bg-slate-100 dark:bg-slate-800 transition ease-in-out delay-30 hover:-translate-y-1 hover:scale-105 hover:shadow-md">
+            <div className="flex flex-col flex-auto px-6 py-4">
+              <p className="mb-3 text-sm text-slate-500">{item.group}</p>
+              <p className="text-xl font-bold text-slate-700 leading-tight">{item.title}</p>
+              <p className="mt-auto text-sm text-slate-500">{item.service}</p>
+            </div>
+            <div className="h-3/6 mt-auto bg-slate-200">
+              <img src={item.img} className="w-full min-h-full" alt="" />
+            </div>            
+          </a>
+        ))}
       </div>
-    </main>
+    </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -7,10 +7,10 @@ export default async function Home() {
   return (
     <main className="">
       <div className="">
-        <div className="max-w-7xl mx-auto py-14 grid grid-cols-4 grid-flow-row gap-10">
+        <div className="max-w-7xl mx-auto py-14 grid sm:grid-cols-4 sm:grid-flow-row sm:gap-10">
           <div className="relative flex flex-col items-center justify-center p-3 col-start-1 col-end-3 row-start-1 row-end-3">
-            <p className="text-4xl dark:sm:text-slate-400">매장운영의 기본 중 기본!</p>
-            <p className="mt-1 text-6xl font-semibold dark:sm:text-white font-co">투게더 포스 솔루션</p>
+            <p className="text-2xl sm:text-4xl dark:sm:text-slate-400">매장운영의 기본 중 기본!</p>
+            <p className="mt-1 text-4xl font-semibold sm:text-6xl dark:sm:text-white font-co">투게더 포스 솔루션</p>
             <p className="mt-1 text-sm leading-6 col-start-1 sm:col-span-2 xl:mt-6 xl:row-start-4 xl:col-span-1 dark:text-slate-400">
               일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십
             </p>
